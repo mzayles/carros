876362badc82219b8bd0d43272edec5d1c72ca0d
Mariana Alves <marisouza0230@gmail.com>
2024-03-24 11:54:16 -0300

    projeto finalizado (correção da borda)

--- src/components/Luxuosos.jsx
+++ src/components/Luxuosos.jsx
@@ -2,7 +2,7 @@ import iconLuxury from "../assets/icon-luxury.svg"
 
 export default function Luxuosos() {
   return (
-        <section className="bg-dark-green p-12 max_lg:rounded-b-lg lg:rounded-r-lg">
+        <section className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
             <img src={iconLuxury} alt="ícone dos carros de luxo" className="mb-7" />
             <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
             <p className="font-lexend mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
